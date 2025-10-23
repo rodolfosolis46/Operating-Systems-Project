552878f4d753d9b9949133867651bb0cea902c58
rodolfosolis46 <kier5120@gmail.com>
2025-10-23 01:18:07 -0500

    Add files via upload

---
+++ process.cpp
@@ -0,0 +1,57 @@
+#include "process.h"
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+Process::Process(int pid, int arrivalTime, int burstTime, int priority, int memory, const vector<int>& ioOperations)
+    : pid(pid), state(ProcessState::NEW), arrivalTime(arrivalTime), burstTime(burstTime), remainingTime(burstTime),
+      waitingTime(0), turnAroundTime(0), priority(priority), memoryRequired(memory),
+      ioOperations(ioOperations) {}
+
+void Process::set_state(const string& newState, int currentTime) {
+    state = newState;
+    if(state == ProcessState::TERMINATED && currentTime != -1){
+        processDone(currentTime);
+    }
+}
+
+bool Process::tickCPU(int cpuTime, int currentTime){
+    if(state != ProcessState::RUNNING) return false;
+    if(remainingTime <= 0 || cpuTime <= 0) return false;
+
+    int timeSpent = min(remainingTime, cpuTime);
+    remainingTime -= timeSpent;
+
+    if(remainingTime == 0){
+        set_state(ProcessState::TERMINATED, currentTime);
+        return true;
+    }
+    return false;
+
+}
+
+void Process::tickWait(int cpuTime){
+    waitingTime += cpuTime;
+}
+
+void Process::processDone(int currentTime){
+    turnAroundTime = currentTime - arrivalTime;
+}
+
+void Process::summary() const {
+    cout << "Process Summary for PID=" << pid << ":\n";
+    cout << "State: " << state << "\n";
+    cout << "Arrival Time: " << arrivalTime << "\n";
+    cout << "Burst Time: " << burstTime << "\n";
+    cout << "Remaining Time: " << remainingTime << "\n";
+    cout << "Waiting Time: " << waitingTime << "\n";
+    cout << "Turnaround Time: " << turnAroundTime << "\n";
+    cout << "Priority: " << priority << "\n";
+    cout << "Memory Required: " << memoryRequired << "\n";
+    cout << "I/O Operations: ";
+    for (const auto& io : ioOperations) {
+        cout << io << " ";
+    }
+    cout << "\n";
+}
---
+++ process.h
@@ -0,0 +1,56 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <string>
+#include <vector>
+#include <ostream>
+using namespace std;
+
+namespace ProcessState{
+    static const string NEW = "NEW";
+    static const string READY = "READY";
+    static const string RUNNING = "RUNNING";
+    static const string WAITING = "WAITING";
+    static const string TERMINATED = "TERMINATED";
+}
+
+class Process{
+public:
+    Process(int pid, int arrivalTime, int burstTime, int priority = 0, int memory = 0, const vector<int>& ioOperations = {});
+
+    void set_state(const string& newState, int currentTime = -1);
+    bool tickCPU(int cpuTime = 1, int currentTime = -1); 
+    void tickWait(int cpuTime = 1);
+
+
+    // Getter functions
+    int getPid() const {return pid; }
+    string getState() const { return state; }
+    int getArrivalTime() const { return arrivalTime; }
+    int getBurstTime() const { return burstTime; }
+    int getRemainingTime() const { return remainingTime; }
+    int getWaitingTime() const { return waitingTime; }
+    int getTurnAroundTime() const { return turnAroundTime; }
+    int getPriority() const { return priority; }
+    int getMemoryRequired() const { return memoryRequired; }
+    const vector<int>& getIoOperations() const { return ioOperations; }
+
+    void summary() const;
+
+
+private:
+    int pid;
+    string state;
+    int arrivalTime;
+    int burstTime;
+    int remainingTime;
+    int waitingTime;
+    int turnAroundTime;
+    int priority;
+    int memoryRequired;
+    vector<int> ioOperations;
+
+    void processDone(int currentTime);
+};
+
+#endif
